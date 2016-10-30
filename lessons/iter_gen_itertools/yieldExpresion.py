ecf4761763cadade425bb7ce94c5e9437442692d
ioMyName <meageinsttoworld@gmail.com>
2016-10-31 01:28:23 +0200

    gen.send(res)      =      next(gen)

--- lessons/iter_gen_itertools/yieldExpresion.py
+++ lessons/iter_gen_itertools/yieldExpresion.py
@@ -5,6 +5,9 @@ def g():
     print("New Got {!r}".format(res))
 
 gen = g()
-next(gen)
-next(gen)
-next(gen)
+# next(gen)
+# next(gen)
+# next(gen)     return None
+
+gen.send(None)  # = next(gen)
+gen.send("foobar")
