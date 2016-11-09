367ed962889bb12051a64fae8791d8fc0cc756fb
ioMyName <meageinsttoworld@gmail.com>
2016-11-09 17:55:07 +0200

    import contacts ---> @post, @pre deco handmahe @pre

--- lessons/decorate/pre.py
+++ lessons/decorate/pre.py
@@ -1,7 +1,25 @@
-import contacts
+'''import contacts
 
 
 @pre(lambda _,y: len(y) > 0)
 def find_element(elem,list):
     pass
 
+'''
+from functools import wraps
+
+def pre(cond, message):
+    def wrapper(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            assert cond(*args, **kwargs), message
+            return func(*args, **kwargs)
+        return inner
+    return wrapper
+
+@pre(lambda x: x >=0, 'negative arg')
+def check_numb(x):
+    return x
+
+
+check_numb(42)
